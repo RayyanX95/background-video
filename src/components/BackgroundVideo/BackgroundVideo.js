03a8273afe2452442d301a4e12455466ee490882
Rayyan995 <ibrahim.alrayany@gmail.com>
2019-09-15 09:07:56 +0200

    second commit

--- src/components/BackgroundVideo/BackgroundVideo.js
+++ src/components/BackgroundVideo/BackgroundVideo.js
@@ -16,6 +16,9 @@ const BackgroundVideo = () => {
                     <h1>Reactjs Course</h1>
                     <p>Learn how to develope React projects</p>
                     <button type="button" className="btn btn-outline-dark">View the course</button>
+                    <img
+                        src="https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                        alt="profile" />
                 </div>
             </div>
         </div>
