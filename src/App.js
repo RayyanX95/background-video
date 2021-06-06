1a4e4708cb84e067fee02a272328ead62cfc030b
Ibarhim <ibrahim.alrayany@gmail.com>
2021-06-07 00:57:32 +0200

    Pass overlay content as children

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ import BackgroundVideo from './components/BackgroundVideo/BackgroundVideo';
 import './App.css';
 
 function App() {
+  const scrollToBottom = () => {
+    const bottomEle = document.getElementById("video-bottom");
+    bottomEle.scrollIntoView({ behavior: "smooth" });
+  }
+  
   return (
-    <div style={{ height: '',}} >
-      <BackgroundVideo />
+    <div style={{ height: '', }} >
+      <BackgroundVideo>
+        <div className='content'>
+          <div className='sub-content' >
+            <h1>Reactjs Course</h1>
+            <p>Learn how to develope React projects</p>
+            <button
+              className="btn btn-outline-dark"
+              onClick={scrollToBottom}>
+              Let's go!
+          </button>
+            <img
+              className="view-image"
+              src="https://www.jing.fm/clipimg/detail/139-1394959_panda-cartoon-png-cute-cartoon-panda-bear.png"
+              alt="profile" />
+          </div>
+        </div>
+      </BackgroundVideo>
 
       <div style={{ height: 1500, backgroundColor: '#60255f' }} >
       </div>
--- src/components/BackgroundVideo/BackgroundVideo.js
+++ src/components/BackgroundVideo/BackgroundVideo.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
 const BackgroundVideo = (props) => {
-
-  const videoSource = "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
-  const scrollToBottom = () => {
-    const bottomEle = document.getElementById("video-bottom");
-    bottomEle.scrollIntoView({ behavior: "smooth" });
-    debugger
-  }
+  const videoSource = "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4";
 
   return (
     <>
@@ -20,22 +14,7 @@ const BackgroundVideo = (props) => {
           <source src={videoSource} type="video/mp4" />
             Your browser does not support the video tag.
       </video>
-
-        <div className='content'>
-          <div className='sub-content' >
-            <h1>Reactjs Course</h1>
-            <p>Learn how to develope React projects</p>
-            <button
-              className="btn btn-outline-dark"
-              onClick={scrollToBottom}>
-              Let's go!
-          </button>
-            <img
-              className="view-image"
-              src="https://www.jing.fm/clipimg/detail/139-1394959_panda-cartoon-png-cute-cartoon-panda-bear.png"
-              alt="profile" />
-          </div>
-        </div>
+        {props.children}
       </div>
       <span id="video-bottom"></span>
     </>
