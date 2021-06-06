3d7cedd1cbe48e8c8c362ac8e801bb025f0e5fb2
Ibarhim <ibrahim.alrayany@gmail.com>
2021-06-07 01:32:40 +0200

    remove the maac-height property

--- src/components/BackgroundVideo/BackgroundVideo.js
+++ src/components/BackgroundVideo/BackgroundVideo.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 const BackgroundVideo = ({ videoSource, children }) => {
+  // TODO: Accept `blur` value as a prop
+
   return (
     <>
-      <div className='container' style={{ maxHeight: window.innerHeight }}>
+      <div className='container'>
         <video
           autoPlay="autoplay"
           loop="loop"
           muted
           className='video' >
+            {/* TODO make it accept multiple media types */}
           <source src={videoSource} type="video/mp4" />
             Your browser does not support the video tag.
       </video>
